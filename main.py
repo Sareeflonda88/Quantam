30a54e91d4be6690f5d836555a8538c8355589ef
Vivan0128 <k7055820@gmail.com>
2025-05-18 14:13:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = Client(
     "QuantumRoboticsBot",
     api_id="28362125",
     api_hash="c750e5872a2af51801d9b449983f4c84",
-    bot_token="7038637559:AAHMjoy7ka3cKMnDkIS_N5-lylbuZB9TZww"
+    bot_token="7038637559:AAFmvn2kmNuN2MukROcmc12B2jBgU8WuJGU"
 )
 
 # RapidAPI Configuration
