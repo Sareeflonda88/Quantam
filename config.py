b44d17509fd5df4b063928c3e17b89f911eaa98f
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2025-05-18 20:43:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ API_ID = os.getenv("API_ID")
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-API_KEY = os.getenv("API_KEY")
-BASE_URL = os.getenv("BASE_URL", "https://api.together.xyz/v1/chat/completions")
+API_KEY = "xxx" ## API KEY FROM https://rapidapi.com/okgpt-ai/api/okai
+BASE_URL = "https://okai.p.rapidapi.com/v1/chat" ##  Api endpoint: https://rapidapi.com/okgpt-ai/api/okai
+AI_MODEL = "quantum"
 
 SUPPORT_LINK = os.getenv("SUPPORT_LINK", "https://t.me/Exampurrs")
 UPDATES_LINK = os.getenv("UPDATES_LINK", "https://t.me/APNA_SYSTEM")
